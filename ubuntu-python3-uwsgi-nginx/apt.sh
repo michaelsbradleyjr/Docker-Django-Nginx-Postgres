ececf1ef6f7e1c2c98e5020e7d8b13736ae15b2c
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2017-09-17 17:56:28 -0500

    more tools

--- ubuntu-python3-uwsgi-nginx/apt.sh
+++ ubuntu-python3-uwsgi-nginx/apt.sh
@@ -6,16 +6,22 @@ set -x #echo on
 apt-get update
 apt-get upgrade -y
 apt-get install -y \
+        bash-completion \
         build-essential \
         ca-certificates \
         curl \
         ed \
+        emacs-nox \
         git \
         mg \
         nginx \
+        parallel \
         python3-dev \
         python3-venv \
+        screen \
+        silversearcher-ag \
         supervisor \
+        tmux \
         vim
 
 rm -rf /var/lib/apt/lists/*
