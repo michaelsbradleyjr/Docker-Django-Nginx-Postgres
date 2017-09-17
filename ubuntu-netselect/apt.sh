6daf0437dd98cbf0d203c53d6cd1749749676860
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2017-09-17 17:55:09 -0500

    locale, LANG

--- ubuntu-netselect/apt.sh
+++ ubuntu-netselect/apt.sh
@@ -4,6 +4,7 @@ set -x #echo on
 apt-get update
 apt-get upgrade -y
 apt-get install -y \
+        locales \
         wget
 
 rm -rf /var/lib/apt/lists/*
--- ubuntu-netselect/build.sh
+++ ubuntu-netselect/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -x #echo on
 
+locale-gen --purge "en_US.UTF-8"
+cat /build/ubuntu-netselect/default_locale > /etc/default/locale
+dpkg-reconfigure locales
+
 wget -O netselect.deb http://http.us.debian.org/debian/pool/main/n/netselect/netselect_0.3.ds1-28+b1_`dpkg --print-architecture`.deb
 
 dpkg -i netselect.deb
